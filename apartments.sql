241b6bb8820f06705914f58599ae63ce32c4de17
Solomiya <solomiya04@gmail.com>
2023-06-10 22:23:08 +0300

    fix apartments.sql

--- apartments.sql
+++ apartments.sql
@@ -1,12 +1,12 @@
 
-insert into sharing.apartments(apartment_id, area, image_url, location, price, purchaser_id, quarter, report_message, rooms, status, floor) VALUES (1,75,'https://cdn.riastatic.com/photos/dom/photo/21171/2117143/211714364/211714364fm.jpg','Сахарова 23', 10000, 2, 'Франківський','', 3, 0, 1);
+insert into sharing.apartments(apartment_id, area, image_url, location, price, quarter, report_message, rooms, status, floor) VALUES (1,75,'https://cdn.riastatic.com/photos/dom/photo/21171/2117143/211714364/211714364fm.jpg','Сахарова 23', 10000, 'Франківський','', 3, 0, 1);
 
-insert into sharing.apartments(apartment_id, area, image_url, location, price, purchaser_id, quarter, report_message, rooms, status, floor) VALUES (2,86,'https://miete.com.ua/apartment_thumbnails/8ff0e4c7b9e1babc4264791f57e44dda.jpg','Шевченка 3', 12300, 1, 'Шевченківський','', 4, 0, 4);
+insert into sharing.apartments(apartment_id, area, image_url, location, price, quarter, report_message, rooms, status, floor) VALUES (2,86,'https://miete.com.ua/apartment_thumbnails/8ff0e4c7b9e1babc4264791f57e44dda.jpg','Шевченка 3', 12300, 'Шевченківський','', 4, 0, 4);
 
-insert into sharing.apartments(apartment_id, area, image_url, location, price, purchaser_id, quarter, report_message, rooms, status, floor) VALUES (3,52,'https://miete.com.ua/apartment_thumbnails/ba3751ee3165214b92502a9459a3942b.jpg','Личаківська 5', 9800, 3, 'Личаківський','', 2, 0, 9);
+insert into sharing.apartments(apartment_id, area, image_url, location, price, quarter, report_message, rooms, status, floor) VALUES (3,52,'https://miete.com.ua/apartment_thumbnails/ba3751ee3165214b92502a9459a3942b.jpg','Личаківська 5', 9800, 'Личаківський','', 2, 0, 9);
 
-insert into sharing.apartments(apartment_id, area, image_url, location, price, purchaser_id, quarter, report_message, rooms, status, floor) VALUES (4,38,'https://miete.com.ua/apartment_thumbnails/286496734765be2aa467de556c717193.jpg','Червоної калини 6', 7000, 4, 'Сихівський','', 1, 0, 7);
+insert into sharing.apartments(apartment_id, area, image_url, location, price, quarter, report_message, rooms, status, floor) VALUES (4,38,'https://miete.com.ua/apartment_thumbnails/286496734765be2aa467de556c717193.jpg','Червоної калини 6', 7000, 'Сихівський','', 1, 0, 7);
 
-insert into sharing.apartments(apartment_id, area, image_url, location, price, purchaser_id, quarter, report_message, rooms, status,floor) VALUES (5,48,'https://miete.com.ua/apartment_thumbnails/fa7b64be1193dbf62e53e7d259ac659b.jpg','Немирівська 7', 8200, 5, 'Залізничний','', 2, 0, 4);
+insert into sharing.apartments(apartment_id, area, image_url, location, price, quarter, report_message, rooms, status,floor) VALUES (5,48,'https://miete.com.ua/apartment_thumbnails/fa7b64be1193dbf62e53e7d259ac659b.jpg','Немирівська 7', 8200,'Залізничний','', 2, 0, 4);
 
-insert into sharing.apartments(apartment_id, area, image_url, location, price, purchaser_id, quarter, report_message, rooms, status, floor) VALUES (6,52,'https://miete.com.ua/apartment_thumbnails/9bcec10a9d7573a684a80796d4063d95.jpg','Куліша 1', 10950, 6, 'Галицький','', 2, 0, 2);
+insert into sharing.apartments(apartment_id, area, image_url, location, price, quarter, report_message, rooms, status, floor) VALUES (6,52,'https://miete.com.ua/apartment_thumbnails/9bcec10a9d7573a684a80796d4063d95.jpg','Куліша 1', 10950,'Галицький','', 2, 0, 2);
